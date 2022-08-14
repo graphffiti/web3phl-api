cda3b12b459368cb03893b65de4f1c80c55f850f
Kristian Quirapas <kmquirapas@gmail.com>
2022-08-14 22:58:16 +0800

    add CORS support

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const directoryRouter = require("./routes/directory");
 
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
